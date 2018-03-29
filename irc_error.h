c913b99a8f89862ebc0b6418ff74fd24d2e69586
IhateProprietary <death2511@hotmail.com>
2018-03-30 00:39:07 +0200

    all error done

--- irc_error.h
+++ irc_error.h
@@ -1,17 +1,17 @@
 #ifndef __IRC_ERROR_H__
 # define __IRC_ERROR_H__
 
-#include "irc_reply.h"
+# include "irc_reply.h"
 
-# define ERR_UNKNOW		0			/* undefined */
-# define ERR_NOSUCHNICK 401 		/* nickname is unsused */
-# define ERR_NOSUCHSERVER 402		/* server doesn't exist */
-# define ERR_NOSUCHCHANNEL 403		/* channel doesn't exist */
-# define ERR_CANNOTSENDTOCHAN 404	/* cannot send to channel */
-# define ERR_TOOMANYCHANNEL 405 	/* max allowed channel joined reached */
-# define ERR_WASNOSUCHNICK 406		/* nickname was never used */
-# define ERR_TOOMANYTARGETS 407		/* duplicate recipient */
-# define ERR_NOORIGIN 409			/* no origin specified */
+# define ERR_UNDEFINED 0
+# define ERR_NOSUCHNICK 401
+# define ERR_NOSUCHSERVER 402
+# define ERR_NOSUCHCHANNEL 403
+# define ERR_CANNOTSENDTOCHAN 404
+# define ERR_TOOMANYCHANNEL 405
+# define ERR_WASNOSUCHNICK 406
+# define ERR_TOOMANYTARGETS 407
+# define ERR_NOORIGIN 409
 # define ERR_NORECIPIENT 411
 # define ERR_NOTEXTTOSEND 412
 # define ERR_NOTOPLEVEL 413
@@ -49,6 +49,6 @@
 # define ERR_UMODEUNKNOWNFLAG 501
 # define ERR_USERSDONTMATCH 502
 
-extern irc_no_t	irc_errstring[];
+extern irc_no_t	*irc_errstring[];
 
 #endif
