91d9ec0264bf19d31f74a719d867f23a45182720
IhateProprietary <death2511@hotmail.com>
2018-04-17 01:26:42 +0200

    hashlib ready

--- hashlib.c
+++ hashlib.c
@@ -3,11 +3,13 @@
 
 #include "hashlib.h"
 
-HASHTABLE	*hash_init(size_t hsize)
+HASHTABLE	*hash_init(int hsize)
 {
 	HASHTABLE	*table;
 	BUCKET		**buckets;
 
+	if (hsize <= 0)
+		return (HASHTABLE *)0;
 	if ((table = (HASHTABLE *)malloc(sizeof *table)) == (HASHTABLE *)0)
 		return (HASHTABLE *)0;
 	if ((buckets = (BUCKET **)malloc(sizeof *buckets * hsize)) == (BUCKET **)0)
@@ -61,6 +63,93 @@ BUCKET		*hash_insert(HASHTABLE *table, char *key, int mode)
 		buck->next = fitem;
 	}
 	return buck;
+}
+
+void		hash_delete(HASHTABLE *table, char *key, void (*del)())
+{
+	BUCKET		*buck;
+	BUCKET		*prev;
+	BUCKET		**head;
+	uint32_t	khash;
+
+	khash = hash_string(key);
+	head = table->buckets + (khash % table->nbuckets);
+	buck = *head;
+	while (buck && buck->khash != khash)
+	{
+		prev = buck;
+		buck = buck->next;
+	}
+	if (buck)
+	{
+		if (buck == *head)
+			*head = buck->next;
+		else
+			prev->next = buck->next;
+		if (del)
+			del(buck->object);
+		free(buck);
+	}
+}
+
+void		hash_destroytable(HASHTABLE *table, void (*del)())
+{
+	BUCKET	**bucks;
+	int		nbuckets;
+
+	bucks = table->buckets;
+	nbuckets = table->nbuckets;
+	for (int i = 0; i < nbuckets; i++)
+	{
+		BUCKET	*buck = bucks[i];
+		BUCKET	*next;
+
+		while (buck)
+		{
+			next = buck->next;
+			if (del)
+				del(buck->object);
+			free(buck);
+			buck = next;
+		}
+	}
+	free(table->nbuckets);
+	free(table);
+}
+
+int			hash_resizetable(HASHTABLE *table, int hsize)
+{
+	BUCKET	**bucks;
+	BUCKET	**cpbucks;
+	int		nbuckets;
+
+	if (hsize <= 0)
+		return 1;
+	if ((bucks = (BUCKET **)malloc(sizeof *bucks * hsize)) == (BUCKET **)0)
+		return 1;
+	cpbucks = table->buckets;
+	nbuckets = table->nbuckets;
+	for (int i = 0; i < hsize; i++)
+		bucks[i] = (BUCKET *)0;
+	for (int i = 0; i < nbuckets; i++)
+	{
+		BUCKET	*buck = cpbucks[i];
+		BUCKET	*next;
+		int		npos;
+
+		while (buck)
+		{
+			next = buck->next;
+			npos = buck->khash % hsize;
+			buck->next = bucks[npos];
+			bucks[npos] = buck;
+			buck = next;
+		}
+	}
+	table->nbuckets = hsize;
+	table->buckets = bucks;
+	free(cpbucks);
+	return (0);
 }
 
 uint32_t	hash_string(char *s)
--- hashlib.h
+++ hashlib.h
@@ -21,9 +21,12 @@ typedef struct
 	int		nbuckets;
 }	HASHTABLE;
 
-HASHTABLE	*hash_init(size_t hsize);
+HASHTABLE	*hash_init(int hsize);
 BUCKET		*hash_search(HASHTABLE *table, char *key);
 BUCKET		*hash_insert(HASHTABLE *table, char *key, int mode);
+int			hash_resizetable(HASHTABLE *table, int hsize);
+void		hash_destroytable(HASHTABLE *table, void (*del)());
+void		hash_delete(HASHTABLE *table, char *key, void (*del)());
 uint32_t	hash_string(char *key);
 
 #endif
